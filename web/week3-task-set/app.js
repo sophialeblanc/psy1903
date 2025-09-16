9e6ae256ce45d9fb04d17762da3ee3c2299efa71
Sophia LeBlanc <sophialeblanc@college.harvard.edu>
2025-09-16 16:55:33 -0400

    Week 3 Q7 Response

--- web/week3-task-set/app.js
+++ web/week3-task-set/app.js
@@ -1,4 +1,4 @@
-// Identify elements we want to update
+/* // Identify elements we want to update
 
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
@@ -11,4 +11,40 @@ let randomNum2 = Math.floor(Math.random() * 10) + 1;
 // Update elements on the page
 
 num1.innerHTML = randomNum1;
-num2.innerHTML = randomNum2;
+num2.innerHTML = randomNum2; */
+
+/* let response = prompt('What is your name?');
+let count = response.length
+console.log(count)
+let firstLetter = response.charAt(0);
+console.log(firstLetter)
+let lastLetter = response.charAt(count - 1);
+console.log(lastLetter) */
+
+/* let age = 20;
+if (age == 25) {
+    console.log("Age is 25");
+} */
+
+//Setting numerical values to random
+let randomNum1 = Math.floor(Math.random() * 10) + 1;
+let randomNum2 = Math.floor(Math.random() * 10) + 1;
+
+//Defining variable/prompt using random numbers
+let response = prompt('What is ' + randomNum1 + ' + ' + randomNum2 + '?');
+
+//Setting more variables for ease of use
+let feedback = '';
+let correctAnswer = randomNum1 + randomNum2
+
+//Determining if/else conditions
+if (response == correctAnswer) {
+    feedback = 'Correct!';
+} else if (response == correctAnswer - 1 || response == correctAnswer + 1) {
+    feedback = 'You were close!';
+} else {
+    feedback = 'Incorrect.';
+}
+
+//Universal feedback
+alert(feedback + ' The expected answer is ' + correctAnswer + '.');
