aa85d0bbd607e9d03aeb85cfab1bedea80ae0399
Sophia LeBlanc <sophialeblanc@college.harvard.edu>
2025-09-23 16:17:08 -0400

    Week 4 Q2

---
+++ web/week4-task-set/app.js
@@ -0,0 +1,6 @@
+function celsiusToFahrenheit(c) {
+    return (c * 1.8) + 32;
+}
+
+console.log(celsiusToFahrenheit(10)); // Expected output: 50
+console.log(celsiusToFahrenheit(-5)); // Expected output: 23
